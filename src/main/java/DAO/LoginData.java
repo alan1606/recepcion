4df1bd1c4a278f62021caaff1525acebb3bc229f
alan1606 <mauri.ar3@gmail.com>
2021-11-11 13:11:43 -0700

    Modifiqué el pom para entrar sin VPN

--- src/main/java/DAO/LoginData.java
+++ src/main/java/DAO/LoginData.java
@@ -12,7 +12,7 @@ package DAO;
 public class LoginData {
     public static final String user = "admin";
     public static final String password = "O9CavZ$cYSlKbk1^m%Q!t8OpaIg5hAIT$fjJR4Hh6VJYVr9xCZ";
-    public static final String serverApiAddress = "http://172.17.200.61:8080/api-wl/webservice";
+    public static final String serverApiAddress = "http://201.116.155.166:9999/api-wl/webservice";
     
      //public static final String user = "admin";
    // public static final String password = "admin";
